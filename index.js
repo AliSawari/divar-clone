76d5e4e4dbd3147cb04116ef7e1dc70397766ef5
Ali Sawari <ali.dragon.swr@gmail.com>
2018-12-20 12:53:57 +0330

    Ok next stage

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+// importing libs
 const express = require('express')
 const app = express()
 const fs = require('fs')
 const bp = require('body-parser')
+const expHbr = require('express-handlebars')
 
+// custom
+const public = `${__dirname}/public`
+const file = f =>  `${public}/${f}`
 const User = require('./helpers/user')
 
+// configuring server
+app.use(express.static(public))
+app.engine('handlebars', expHbr({defaultLayout: file('main')}))
+app.set('view engine', 'handlebars')
 app.use(bp.json())
 
+// routes
 app.all('/', (req,res) => {
-  res.send("Welcome to Divar Clone ")
+  res.render(file('home'), {
+    title: 'Divar Home'
+  })
 })
 
 app.post('/api/user', (req, res) => {
@@ -18,6 +30,7 @@ app.post('/api/user', (req, res) => {
   res.send("success: user has been saved!")
 })
 
+// starting server
 app.listen(3000, err => {
   if (err) return err
   console.log("Server is Running on port 3000")
