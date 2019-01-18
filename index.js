0439612e0306a485eb3eeeeee2c510ab99330d34
mrghost <ali.dragon.swr@gmail.com>
2019-01-18 17:15:56 +0330

    new routes

--- index.js
+++ index.js
@@ -7,49 +7,45 @@ const expHbr = require('express-handlebars')
 // custom
 const public = `${__dirname}/public`
 const file = f =>  `${public}/${f}`
-const {loadData, saveData} = require('./helpers/io')
+const {loadData, saveData, findOne} = require('./helpers/io')
 const Post = require('./helpers/post')
 
 // configuring server
 app.use(express.static(public))
 app.engine('handlebars', expHbr({defaultLayout: file('main')}))
 app.set('view engine', 'handlebars')
-app.use(bp.json())
+app.use(bp.urlencoded({extended:true}))
 
-// routes
-app.all('/', (req,res) => {
-  let all = loadData()
+
+app.get('/', (req,res) => {
+  let data = loadData()
   res.render(file('home'), {
-    title: 'Divar Home',
-    data: all
+    title: 'Home', data
   })
 })
 
-app.get('/product/:id', (req, res) => {
+app.get('/product/:id', (req,res) => {
   let {id} = req.params
-  let all = loadData()
-  res.render(file('product'), {
-    title: id
-  })
+  let p = findOne(id)
+  if(p){
+    res.render(file('product'), {
+      product: p
+    })
+  } else {
+    res.status(404).render(file('error'), {title:'page not found'})
+  }
 })
 
-app.get('/new', (req, res) => {
-  res.render(file('new'), {
-    title: "New Post"
-  })
-})
+app.get('/new', (req,res) => res.render(file('new'), {title:"new post"}))
 
-app.post('/newPost', (req, res) => {
+app.post('/newpost', (req,res) => {
   let all = loadData()
-  let {title, disc, pics} = req.body
-  let p = new Post(title, disc, pics)
+  let {productName,manuName,price,description} = req.body
+  let p = new Post(`${manuName} - ${productName}`, price, description)
   all.posts.push(p)
   saveData(all)
-  setTimeout(() => {
-    res.redirect('/')
-  }, 500)
+  res.redirect('/')
 })
-
 
 // starting server
 app.listen(3000, err => {
