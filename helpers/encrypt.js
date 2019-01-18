86a691f84b9e1895ecdcd2da7cea83255f6f0c36
mrghost <ali.dragon.swr@gmail.com>
2019-01-18 13:25:48 +0330

    Kim-Jon

--- helpers/encrypt.js
+++
@@ -1,7 +0,0 @@
-const {createHmac} = require('crypto')
-
-function encrypt(data){
-  return createHmac('sha256', data).digest('hex')
-}
-
-module.exports = encrypt
--- helpers/post.js
+++ helpers/post.js
@@ -1,6 +1,6 @@
 class Post {
   constructor(title,disc,pics){
-    this.id = (Math.random() * 100) / (Math.random() * 10)
+    this.id = Math.floor((Math.random() * 1000) / 3.1415) 
     this.title = title
     this.disc = disc,
     this.pics = pics
